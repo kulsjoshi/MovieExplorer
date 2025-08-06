cf76a1034232dbafddb13beb7c97fd29cab10b39
Kuldeep Joshi <kuls.joshi@hotmail.com>
2025-08-06 15:45:08 -0700

    added hilt dependency

--- app/src/main/java/com/kuldeep/movieexplorer/MovieExplorerApp.kt
+++ app/src/main/java/com/kuldeep/movieexplorer/MovieExplorerApp.kt
@@ -1,4 +1,8 @@
 package com.kuldeep.movieexplorer
 
-class MovieExplorerApp {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieExplorerApp : Application() {
 }
---
+++ app/src/main/java/com/kuldeep/movieexplorer/presentation/ui/screens/trending/TrendingScreen.kt
@@ -0,0 +1,10 @@
+package com.kuldeep.movieexplorer.presentation.ui.screens.trending
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.kuldeep.movieexplorer.presentation.viewmodel.MovieViewModel
+
+@Composable
+fun TrendingScreen(viewModel: MovieViewModel){
+
+}
